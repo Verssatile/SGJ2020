b4790a7748bc80f7d001b3e2035982696d0402e5
LelkaSMustak69420 <vesko982@gmail.com>
2020-01-31 19:53:39 +0200

    scene with basic scripts

---
+++ Assets/Scripts/Character/CharacterMovements.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMovements : MonoBehaviour
+{
+    private float speed = 1f;
+
+    private Rigidbody rb;
+
+    NavMeshAgent nav;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        nav = GetComponent<NavMeshAgent>();
+    }
+    #region rigidbody usage
+    private void Move(Vector3 direction)
+    {
+        
+        rb.MovePosition(transform.position + direction*Time.deltaTime);
+    }
+    #endregion
+    #region navmesh usage
+    private void MoveWithNavMesh(Vector3 endPos)
+    {
+        nav.SetDestination(endPos);
+    }
+
+
+    #endregion
+}
---
+++ Assets/Scripts/Menu/MenuButtonsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtonsController : MonoBehaviour
+{
+    Button startGameButton;
+    Button highScoreButton;
+
+
+
+    void Start()
+    {
+        startGameButton = GameObject.Find("StartGame").GetComponent<Button>();
+        highScoreButton = GameObject.Find("HighScore").GetComponent<Button>();
+
+        startGameButton.onClick.AddListener(StartGame);
+        highScoreButton.onClick.AddListener(ShowHighScore);
+    }
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    private void ShowHighScore()
+    {
+
+    }
+}
